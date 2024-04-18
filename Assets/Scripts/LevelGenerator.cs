96ec00745b20534f365c635868874fc07d7890ac
DaltonChris <daltonchris2@gmail.com>
2024-04-19 03:35:10 +0930

    Add PopulateRooms method
    Added functionality to add rooms into the grid
    Random positions in levels grid
    Randomised size (within an amount)
    (need to add the overlaps checks)

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -2,27 +2,31 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class LevelGenerator : MonoBehaviour
 {
-    [SerializeField] int GridWidth;
-    [SerializeField] int GridHeight;
+    [SerializeField] int GridWidth; // Width of the grid to generate (To be set in the inspector) (Must be odd)
+    [SerializeField] int GridHeight; // Width of the grid to generate (To be set in the inspector) (Must be odd)
     [SerializeField] int RoomGenAttempts = 300; // How many times we try to generate a room
     [SerializeField] GameObject TilePreFab; // Prefab for the tile obj
     [SerializeField] Tile[,] Tiles;
 
-    int[,] Regions;
-    readonly float LevelGenDelay = 1f;
+    int[,] Regions; // An array of int's for each pos in the grid each int represents a different Region 
+    int CurrentRegion = -1; // Current region (declared at -1, first region to be 0)
+    readonly float LevelGenDelay = 1f; // The delay inbetween each generation action (To better visualise while developing)
+    List<Vector2Int> RoomList = new(); // A list of all grid position that contain a room
 
 
-    // Update is called once per frame
     void Update()
     {
+        #region Wait for SpaceBar before generating
         if (Input.GetKeyDown(KeyCode.Space)) // If spacebar is pressed
         {
             StopAllCoroutines(); // Stop all coroutines
             InitGrid(GridWidth, GridHeight); // Initalise the grid (Start generating)
         }
+        #endregion
     }
 
     // Coroutine to Go through the Level Generation Steps was small pauses between steps
@@ -31,7 +35,7 @@ public class LevelGenerator : MonoBehaviour
         yield return new WaitForSeconds(LevelGenDelay);
         PopulateGrid(TileType.Wall); // Populate grid with Tiles of wall type as default
         yield return new WaitForSeconds(LevelGenDelay);
-        PopulateGrid_WithRooms(); // Populate grid with rooms
+        PopulateGrid_WithRooms(TileType.Path); // Populate grid with rooms
     }
 
     /// <summary>
@@ -73,8 +77,59 @@ public class LevelGenerator : MonoBehaviour
             }
         }
     }
-    void PopulateGrid_WithRooms()
-    {
 
+    /// <summary>
+    /// A method to populate the grid with various randomly sized rooms at random positions
+    /// </summary>
+    /// <param name="roomTile"> The tileType to set the rooms tiles to </param>
+    void PopulateGrid_WithRooms(TileType roomTile)
+    {
+        for (int i = 0; i < RoomGenAttempts; i++)
+        {
+            #region Randomise Room Size / Position
+            // min 7 max 9
+            int size = Random.Range(3, 5) * 2 + 1; // Random range between 3,5 Multiplied by 2 + 1 to keep rooms odd
+            int width = size; // declare width int set to size
+            int height = size / 2; // declare height int set to half size
+
+            // Randomly int the width 50% chance
+            if (Random.Range(0, 2) == 0) width *= 2; // If is 0, double width
+
+            // Randomise x,y position in grid for room to be placed
+            // Random number between 2 and the width of the Levels grid - width (room width) - Do same for height
+            int x = Random.Range(2, GridWidth - width);
+            int y = Random.Range(2, GridHeight - height);
+            #endregion
+
+
+            //Debug log for each room positon and width/height
+            Debug.Log($"Room being added @ - X: {x} Y: {y} Size: Width: {width} Height: {height}");
+            
+            StartRegion(); // Start a new region (Increment current region)
+
+            #region Add the room to the grid
+            // loop through grid positions (starting at the randomise x,y position)
+            for (int yy = y; yy < y + height; yy++) // For each Position in the grids y axis from start pos -> to start pos + height (room height)
+            {
+                for (int xx = x; xx < x + width; xx++) // For each Position in the grids x axis from start pos -> to start pos + width (room width)
+                {
+                    // Set the type of the tile at the current x,y pos (xx,yy) to the passed roomTile var
+                    Tiles[xx, yy].SetType(roomTile);
+                    // Update the Regions array with the new region information the room tile
+                    Regions[xx, yy] = CurrentRegion;
+                    // Add the room position to the RoomList 
+                    RoomList.Add(new Vector2Int(xx, yy));
+                }
+            }
+            #endregion
+        }
+    }
+
+    /// <summary>
+    /// Increments the CurrentRegion int (Each int is a marker of a region)
+    /// </summary>
+    private void StartRegion()
+    {
+        CurrentRegion++; // Increment Current region
     }
 }
