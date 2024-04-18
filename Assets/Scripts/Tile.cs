e54f7d3df4ab3a782d3f33a8b87cb9c017b3b01c
DaltonChris <daltonchris2@gmail.com>
2024-04-19 01:23:33 +0930

    Remove Files / Make Tile class & Prefab
    New plan for levelgen / clean up files, write a simple Tile class and make a prefab for testing

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,14 +1,44 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "WFC Tile", menuName = "WaveFuncCollapse/Tile")]
-public class Tile : ScriptableObject
+// Enum for the Tile Type
+public enum TileType
 {
-    [Header("GameObj to hold sprite's / Map pieces etc.")]
-    public GameObject gameObject;
+    Wall,
+    Path
+}
 
-    [Header("WaveFunc-Collapse - Algorithm tile data [Adjacent-compatabilty]")]
-    public int left;
-    public int right;
-    public int top;
-    public int bottom;
+public class Tile : MonoBehaviour
+{
+    [SerializeField] Sprite WallSprite;
+    [SerializeField] Sprite PathSprite;
+    SpriteRenderer SpriteRenderer;
+    public TileType Type { get; internal set; }
+
+    private void Awake() // On awake get the SpriteRenderer
+    {
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Method to change the type of the tile and update sprite
+    public void SetType(TileType newType)
+    {
+        Type = newType; // Set the Type to the passed sprite
+        UpdateSprite(); // call Update Sprite
+    }
+
+    // Method to update the sprite based on type
+    private void UpdateSprite()
+    {
+        switch (Type) // Switch statement for the Tile Type
+        {
+            case TileType.Wall: // Case wall Type, Set Wall Sprite
+                SpriteRenderer.sprite = WallSprite;
+                break;
+            case TileType.Path: // Case Path Type, Set Wall Sprite
+                SpriteRenderer.sprite = PathSprite;
+                break;
+            default:
+                break;
+        }
+    }
 }
